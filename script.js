548971cea4a20b45333a3ff047a483d6d8beef08
Jorge Capona <jcapona@gmail.com>
2016-01-06 12:15:59 -0300

    Added smoothscrolling to nav buttons

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
-  console.log("Ready!");
-  
-  
   $('#nav').affix({
         offset: {
           top: $("#home").height()
         }
   });
-
   
+  $('body').scrollspy({target: ".nav-buttons", offset: 50});   
+
+  $(".nav-buttons a").on('click', function(event) {
+    event.preventDefault();
+    var hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+      window.location.hash = hash;
+    });
+  });
+
 });
